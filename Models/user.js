0877d795190545ff6b158fe7624bcb144f6a3642
llyman707 <llyman707@gmail.com>
2025-09-04 21:14:17 -0400

    first commit

---
+++ Models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/relationshipDemo')
+    .then(() => {
+        console.log('Mongo connection open')
+    })
+    .catch(err => {
+        console.log('oh no mongo connection error')
+        console.log(err)
+    })
+
+const userSchema = new mongoose.Schema({
+    first: String,
+    last: String,
+    addresses: [
+        {
+            _id: { _id: false },
+            street: String,
+            city: String,
+            state: String,
+            country: String
+        }
+    ]
+})
+
+const User = mongoose.model('User', userSchema);
+
+const makeUser = async () => {
+    const u = new User({
+        first: 'Harry',
+        last: 'Potter'
+    })
+    u.addresses.push({
+        street: '123 Sesame St.',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA'
+    })
+    const res = await u.save()
+    console.log(res)
+}
+
+const addAddress = async (id) => {
+    const user = await User.findById(id);
+    user.addresses.push(
+        {
+            street: '99 3rd St.',
+            city: 'New York',
+            state: 'NY',
+            country: 'USA'
+        }
+    )
+    const res = await user.save()
+    console.log(res);
+}
+
+addAddress('68b5db46f196df6fab800735')
